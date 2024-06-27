38c31c0b38148d32da9cb89a78461c2dfa75207f
nwesha <anwesha11674@gmail.com>
2024-06-27 23:53:16 +0530

    i commit

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { ImSearch } from "react-icons/im";
+import FetchWeather from './components/FetchWeather';
+
+function App() {
+  const [citySearch, setCitySearch] = useState('');
+  const [cityData, setCityData] = useState(null);
+  const [error, setError] = useState(null);
+
+  const fetchCity = (e) => {
+    e.preventDefault();
+    setError(null);
+    axios.get(`http://localhost:4000/api/locations/v1/cities/search?q=${citySearch}`)
+      .then((response) => {
+        if (response.data.length > 0) {
+          setCitySearch('');
+          setCityData(response.data[0]);
+        } else {
+          setError('City not found. Please try something else.');
+          setCityData(null);
+        }
+      })
+      .catch((err) => {
+        console.error('Error fetching city data:', err);
+        setError('There was an error fetching the data.');
+        setCityData(null);
+      });
+  };
+
+  return (
+    <div className="App">
+      <div className="heading">
+      <h1>CityWeather</h1>
+      <h3>Explore weather worldwide!</h3>
+      </div>
+      <div className="form-container">
+        <form className="form-group custom-form" autoComplete="off" onSubmit={fetchCity}>
+          <div className="para">
+          <label>Search any city for instant weather updates.</label>
+          <div className="search-box">
+            <input 
+              type="text" 
+              className="form-control" 
+              required 
+              placeholder="Enter city name..."
+              value={citySearch} 
+              onChange={(e) => setCitySearch(e.target.value)} 
+            />
+          </div>
+          </div>
+          <button type="submit" className="btn"><ImSearch /></button> 
+        </form>
+        {error && <div className="error-box">{error}</div>}
+      </div>
+        <div className="weather-container">
+      {cityData && (
+          <FetchWeather cityData={cityData} />
+        )}
+        </div>
+    </div>  
+  );
+}
+
+export default App;
---
+++ src/components/FetchWeather.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const FetchWeather = ({ cityData }) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      setLoading(true);
+      setError(null);
+
+      try {
+        const locationKey = cityData.Key;
+        const response = await axios.get(`http://localhost:4000/api/currentconditions/${locationKey}`);
+        const weatherData = response.data[0]; // Assuming the first item in the array is sufficient
+        setData(weatherData);
+      } catch (error) {
+        console.error('Error fetching weather data:', error);
+        setError('There was an error fetching the weather data.');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (cityData) {
+      fetchWeatherData();
+    }
+  }, [cityData]);
+  
+
+  const getIconUrl = (iconNumber) => {
+    const iconString = iconNumber.toString().padStart(2, '0');
+    return `https://developer.accuweather.com/sites/default/files/${iconString}-s.png`;
+  };
+
+  if (loading) {
+    return <div className="loader-box">Loading...</div>;
+  }
+
+  if (error) {
+    return <div className="error-box">{error}</div>;
+  }
+
+  return (
+    <>
+      {data && (
+        <main className={`box ${data.IsDayTime ? 'day' : 'night'}`}>
+          <div className={data.IsDayTime ? 'sun animate-sun' : 'moon animate-moon'}></div>
+          <h3 className="location">
+            {cityData.EnglishName} | {cityData.Country.EnglishName}
+          </h3>
+          <div className="details">
+            <p className="weather-text">{data.WeatherText}</p>
+            <img className="weather-img" src={getIconUrl(data.WeatherIcon)} alt="weather icon" />
+            <h2 className="temperature-value">
+              {Math.ceil(data.Temperature?.Metric?.Value)}
+              <sup className="deg">&deg;{data.Temperature?.Metric?.Unit}</sup>
+            </h2>
+          </div>
+        </main>
+      )}
+    </>
+  );
+};
+
+export default FetchWeather;
